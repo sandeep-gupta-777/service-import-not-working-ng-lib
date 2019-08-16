b8f6eab0bf483d633b5568d6ce021e9b306eee2f
Sandeep Kumar Gupta <sandeepkgupta007@gmail.com>
2019-08-16 23:18:01 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+// import {TestService} from '../../projects/skg-lib-test/src/lib/test.service';
+import {TestService} from 'skg-lib-test/lib/test.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'sandeep-test';
+
+  constructor(private skgLibTestService: TestService) {
+
+  }
+
+  ngOnInit(): void {
+    this.skgLibTestService.showAlert();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {SkgLibTestModule} from 'skg-lib-test';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    (SkgLibTestModule as any).forRoot(),//throws error: ERROR in src/app/app.module.ts(13,22): error TS2339: Property 'forRoot' does not exist on type 'typeof SkgLibTestModule'
+    // SkgLibTestModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
